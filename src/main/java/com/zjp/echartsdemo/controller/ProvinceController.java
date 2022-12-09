eedea1a39e427519cc6bb598424fd403461deb74
antrn <2446026601@qq.com>
2022-12-09 12:20:22 +0800

    update

--- src/main/java/com/zjp/echartsdemo/controller/ProvinceController.java
+++ src/main/java/com/zjp/echartsdemo/controller/ProvinceController.java
@@ -2,6 +2,9 @@ package com.zjp.echartsdemo.controller;
 
 import com.zjp.echartsdemo.entity.Product;
 import com.zjp.echartsdemo.entity.Province;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.zjp.echartsdemo.entity.Class;
 import com.zjp.echartsdemo.service.ClassService;
 import com.zjp.echartsdemo.service.IprovinceService;
@@ -14,14 +17,18 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.w3c.dom.NameList;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
 
 @CrossOrigin
+// @CrossOrigin(origins = "*",maxAge = 3600)
 @Controller
 public class ProvinceController {
     @Autowired
@@ -32,7 +39,7 @@ public class ProvinceController {
     @RequestMapping("/getAllProvince")
     @ResponseBody // 以json格式返回数据
     public List<Province> showData() {
-        List<Province> provinceArrayList = new ArrayList<Province>();
+        List<Province> provinceArrayList = new ArrayList<>();
         provinceArrayList = pService.selectAll();
         return provinceArrayList;
     }
@@ -45,14 +52,13 @@ public class ProvinceController {
     @RequestMapping(value = "/getAllClass", produces = "application/json; charset=utf-8")
     @ResponseBody
     public List<Class> getAll() {
-        List<Class> classes = classService.findAll();
-        return classes;
+        return classService.findAll();
     }
 
     @RequestMapping("/clothes")
     @ResponseBody
     public List<Product> myProject() {
-        ArrayList<Product> productArrayList = new ArrayList<Product>();
+        ArrayList<Product> productArrayList = new ArrayList<>();
         Product product1 = new Product();
         product1.setProductName("袜子");
         product1.setNums(15);
@@ -72,6 +78,35 @@ public class ProvinceController {
         productArrayList.add(product4);
 
         return productArrayList;
+    }
+
+    @RequestMapping("/getny")
+    @ResponseBody
+    public Map<String, Object> getData() {
+        Map<String, Object> map = new HashMap<>();
+        JSONArray list = new JSONArray();
+        JSONObject o1 = new JSONObject();
+        String name = "name";
+        o1.put(name, "大棚数");
+        String value = "value";
+        o1.put(value, 1300);
+        JSONObject o2 = new JSONObject();
+        o2.put(name, "仓库");
+        o2.put(value, 90);
+        JSONObject o3 = new JSONObject();
+        o3.put(name, "养殖基地");
+        o3.put(value, 415);
+        JSONObject o4 = new JSONObject();
+        o4.put(name, "销售中心");
+        o4.put(value, 317);
+        list.add(o1);
+        list.add(o2);
+        list.add(o3);
+        list.add(o4);
+        String[] nameList = { "大棚数", "养殖基地", "仓库", "销售中心" };
+        map.put("data_", list);
+        map.put("names", nameList);
+        return map;
     }
 
     @RequestMapping("/echarts")
